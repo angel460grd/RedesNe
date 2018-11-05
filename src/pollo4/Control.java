0eac6f427bc8862d4e14ea4997c62b11343bcfc1
unknown <you@example.com>
2018-11-05 13:23:40 -0600

    miguel cambio control

--- src/pollo4/Control.java
+++ src/pollo4/Control.java
@@ -10,12 +10,14 @@ import java.util.ArrayList;
 
 public class Control {
     ArrayList<Capa> capas=new ArrayList<>();
-    ArrayList<int[]>patronesEntrada=new ArrayList();
+    ArrayList<int[]>patronesEntrada=new ArrayList(),salidas2=new ArrayList<>();
     int entradas,salidas;
     
     double coeficiente;
     double salidaobtenida,salidaDeseada,gradiante,error,umbral,pez;
     double[]pesos;
+    
+    int nEntradas,nSalidas;
     public void entrenar()
     {
         
@@ -41,6 +43,49 @@ public class Control {
         
         
     }
-    
+       public void arquitectura()
+   {
+       int[] entrada=patronesEntrada.get(0);
+       int nNeuronas=entrada.length,nCapas=(int) Math.sqrt(nEntradas*nSalidas);
+            this.AgragarCapa(nNeuronas,nNeuronas);
+       for (int i = 1; i < nCapas; i++) {
+           this.AgragarCapa(nNeuronas+2,nNeuronas);
+           nNeuronas=nNeuronas+2;
+       }
+       this.AgragarCapa(salidas2.get(0).length,nNeuronas);
+       
+   }
+    public void AgragarCapa(int nNeuronas,int entradas)
+    {
+        //esto espara neuronas internas, se debe hacer antes de gragar la capa de salida
+        Capa cn=new Capa();
+        cn.crearNeuronas(nNeuronas,entradas);
+        capas.add(cn);
+        
+    }
+    public void sacarGradante()
+    {
+        Capa capaAnt= capas.get(capas.size()-1);
+        capaAnt.NeuronasGradinateS();
+        for (int i = capas.size()-2; i>=0; i--) {
+            capas.get(i).NeuronasGradinateH(capaAnt);
+            capaAnt=capas.get(i);
+        }
+        //Para sacar el gradiante
+        Neurona n = new Neurona();
+        error= n.CalcularError();
+       
+    }
+    public void entrenar2()
+    {
+        for(int i=0;i<patronesEntrada.size();i++)
+        {
+            capas.get(0).valorEntrada(patronesEntrada.get(i));
+            capas.get(capas.size()-1).crearNeuronasSalida( salidas2.get(i) );
+            
+            
+        }
+        
+    }
     
 }
