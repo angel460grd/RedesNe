48be61069984f1f51af9c21b983921ca0f27f5b5
Luis Saul Gonzalez Guerra <total.hobbies19@gmail.com>
2018-11-05 19:21:12 -0600

    Modificacione Ahora

--- src/pollo4/Control.java
+++ src/pollo4/Control.java
@@ -3,100 +3,95 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pollo4;
 
 import java.util.ArrayList;
 
 public class Control {
-    ArrayList<Capa> capas=new ArrayList<>();
-    ArrayList<int[]>patronesEntrada=new ArrayList(),salidas2=new ArrayList<>();
-    int entradas,salidas;
-    
+
+    ArrayList<Capa> capas = new ArrayList<>();
+    ArrayList<int[]> patronesEntrada = new ArrayList(), salidas2 = new ArrayList<>();
+    int entradas, salidas;
+
     double coeficiente;
-    double salidaobtenida,salidaDeseada,gradiante,error,umbral,pez;
-    double[]pesos;
-    
-    int nEntradas,nSalidas;
-    public void entrenar()
-    {
-        
+    double salidaobtenida, salidaDeseada, gradiante, error, umbral, pez;
+    double[] pesos;
+
+    int nEntradas, nSalidas;
+
+    public void entrenar() {
+
     }
-    public void AgragarCapa()
-    {
-        
+
+    public void AgragarCapa() {
+
     }
-    
-    public void AjustarPesos(  )
-    {
-        
-        
-        
-        for(int i=0; i<0;i++)
-        {
-            
-        }   
-     
-        
-        
-        
-        
-        
+
+    public void AjustarPesos() {
+
+        for (int i = 0; i < 0; i++) {
+
+        }
+
     }
-       public void arquitectura()
-   {
-       int[] entrada=patronesEntrada.get(0);
-       int nNeuronas=entrada.length,nCapas=(int) Math.sqrt(nEntradas*nSalidas);
-            this.AgragarCapa(nNeuronas,nNeuronas);
-       for (int i = 1; i < nCapas; i++) {
-           this.AgragarCapa(nNeuronas+2,nNeuronas);
-           nNeuronas=nNeuronas+2;
-       }
-       this.AgragarCapa(salidas2.get(0).length,nNeuronas);
-       
-   }
-    public void AgragarCapa(int nNeuronas,int entradas)
-    {
+
+    public void arquitectura() {
+        int[] entrada = patronesEntrada.get(0);
+        int nNeuronas = entrada.length, nCapas = (int) Math.sqrt(nEntradas * nSalidas);
+        this.AgragarCapa(nNeuronas, nNeuronas);
+        for (int i = 1; i < nCapas; i++) {
+            this.AgragarCapa(nNeuronas + 2, nNeuronas);
+            nNeuronas = nNeuronas + 2;
+        }
+        this.AgragarCapa(salidas2.get(0).length, nNeuronas);
+
+    }
+
+    public void AgragarCapa(int nNeuronas, int entradas) {
         //esto espara neuronas internas, se debe hacer antes de gragar la capa de salida
-        Capa cn=new Capa();
-        cn.crearNeuronas(nNeuronas,entradas);
+        Capa cn = new Capa();
+        cn.crearNeuronas(nNeuronas, entradas);
         capas.add(cn);
-        
+
     }
-    public void sacarGradante()
-    {
-        Capa capaAnt= capas.get(capas.size()-1);
+
+    public void sacarGradante() {
+        Capa capaAnt = capas.get(capas.size() - 1);
         capaAnt.NeuronasGradinateS();
-        for (int i = capas.size()-2; i>=0; i--) {
+        for (int i = capas.size() - 2; i >= 0; i--) {
             capas.get(i).NeuronasGradinateH(capaAnt);
-            capaAnt=capas.get(i);
+            capaAnt = capas.get(i);
         }
         //Para sacar el gradiante
         Neurona n = new Neurona();
-        error= n.CalcularError();
-       
+        error = n.CalcularError();
+
     }
-    public void entrenar2()
-    {
-        for(int i=0;i<patronesEntrada.size();i++)
-        {
-            capas.get(0).valorEntrada(patronesEntrada.get(i));
-            capas.get(capas.size()-1).crearNeuronasSalida( salidas2.get(i) );
-            this.sacarGradante();
-            this.evaluar();
-            this.AjustarPesos();
-            
+
+    public void entrenar2() {
+        int iterador = 0;
+        do {
+            for (int i = 0; i < patronesEntrada.size(); i++) {
+                capas.get(0).valorEntrada(patronesEntrada.get(i));
+                capas.get(capas.size() - 1).crearNeuronasSalida(salidas2.get(i));
+                this.sacarGradante();
+                this.evaluar();
+                this.AjustarPesos();
+
+            }
+        } while (iterador == 500);
+
+    }
+
+    public void evaluar() {
+        for (Capa c : capas) {
+            c.evaluarN();
         }
-        
     }
-    public void evaluar()
-    {
-       for(Capa c:capas)
-           c.evaluarN();
-    }
-    public void ajuste()
-    {
-        for(Capa c:capas)
+
+    public void ajuste() {
+        for (Capa c : capas) {
             c.ajustarPesosN();
+        }
     }
 }
